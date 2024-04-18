d56730db15673fef6c3d9b6b9abe687c24446264
diego <diegozaratean@gmail.com>
2024-04-18 02:20:35 +0000

    finish course model

--- src/models.py
+++ src/models.py
@@ -7,26 +7,46 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-
-class Address(Base):
-    __tablename__ = 'address'
+class Teacher(Base):
+    __tablename__ = 'teacher'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250), nullable=False)
+    city = Column(String(250))    
+    age = Column(Integer)
 
-    def to_dict(self):
-        return {}
+
+class Course(Base):
+    __tablename__ = 'course'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    description = Column(String(250))   
+    teacher_id = Column(Integer, ForeignKey('teacher.id'))
+    teacher = relationship(Teacher) 
+
+
+class Student(Base):
+    __tablename__ = 'student'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    city = Column(String(250))    
+    level = Column(String(250))  
+
+class StudentCourse(Base):
+    __tablename__ = 'student_course'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    student = relationship(Student)
+    student_id = Column(Integer, ForeignKey('student.id'))
+    course_id = Column(Integer, ForeignKey('course.id'))
+    course = relationship(Course)
+    
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
